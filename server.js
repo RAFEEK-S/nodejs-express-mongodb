afdf4f75ce118b6e244e7faa5ad3d0ce6a75ad2a
RAFEEK-S <rafeeksarthar73@gmail.com>
2023-02-07 18:10:44 +0530

    server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));nmmmm
+app.use(express.urlencoded({ extended: true }));
 
 
 
